a27b31ef89faaf7e639f2b99975c8bee52a0e902
arttupii <arttupii@gmail.com>
2026-08-26 09:38:48 +0300

    Create laskin.py

---
+++ laskin.py
@@ -0,0 +1,16 @@
+
+def main():
+    print("=== Ryhmän laskin ===")
+    print("1. Yhteenlasku")
+    print("2. Vähennyslasku")
+    print("3. Kertolasku")
+    print("4. Jakolasku")
+
+    valinta = input("Valitse laskutoimitus: ")
+
+    a = float(input("Anna ensimmäinen luku: "))
+    b = float(input("Anna toinen luku: "))
+
+
+if __name__ == "__main__":
+    main()
