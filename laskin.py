a28d2a4c50a43dd3c242dab41c977f6429173cb1
arttupii <arttupii@gmail.com>
2026-08-26 09:49:24 +0300

    Update laskin.py

--- laskin.py
+++ laskin.py
@@ -1,4 +1,18 @@
+def summa(a, b):
+    print("summa", a, b)
 
+
+def erotus(a, b):
+    print("erotus", a, b)
+
+
+def tulo(a, b):
+   print("tulo", a, b)
+
+
+def jako(a, b):
+    print("jako", a, b)
+    
 def main():
     print("=== Ryhmän laskin ===")
     print("1. Yhteenlasku")
@@ -10,6 +24,17 @@ def main():
 
     a = float(input("Anna ensimmäinen luku: "))
     b = float(input("Anna toinen luku: "))
+
+    if valinta == "1":
+        print("Tulos:", summa(a, b))
+    elif valinta == "2":
+        print("Tulos:", erotus(a, b))
+    elif valinta == "3":
+        print("Tulos:", tulo(a, b))
+    elif valinta == "4":
+        print("Tulos:", jako(a, b))
+    else:
+        print("Virheellinen valinta")
 
 
 if __name__ == "__main__":
